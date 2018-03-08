01e6d74d57beeec9f29b304d467fe24b17464e58
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-03-08 13:51:14 -0800

    JS Interop

---
+++ WebApplication1/Models/Browser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace WebApplication1.Models
+{
+    public static class Browser
+    {
+
+        public static string MyAlert(string msg)
+        {
+            return RegisteredFunction.Invoke<string>("MyAlert", msg);
+        }
+    }
+}
